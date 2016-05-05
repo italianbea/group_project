a92c1f06f4933562c5e4a5a09327327be0c7edc9
italianbea <italianbea@gmail.com>
2016-05-05 06:07:16 +0000

    first commit

---
+++ js/bali.js
@@ -0,0 +1,68 @@
+// JavaScript File
+
+//js for memory game
+var memory_array = ['B','B','A','A','L','L','I','I','I','I','N','N','D','D','O','O','N','N','E','E','S','S','A','A'];
+var memory_values = [];
+var memory_tile_ids = [];
+var tiles_flipped = 0;
+//shuffle method
+Array.prototype.memory_tile_shuffle = function(){
+    var i = this.length, j, temp;
+    while(--i > 0){
+        j = Math.floor(Math.random() * (i+1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
+}; /*dont forget the semicolon*/
+//generates new board
+function newBoard(){
+	tiles_flipped = 0;
+	var output = '';
+    memory_array.memory_tile_shuffle();
+	for(var i = 0; i < memory_array.length; i++){
+		output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+	}
+	document.getElementById('memory_board').innerHTML = output;
+}
+
+function memoryFlipTile(tile,val){
+	if(tile.innerHTML == "" && memory_values.length < 2){
+		tile.style.background = '#00cdcd';
+		tile.innerHTML = val;
+		if(memory_values.length == 0){
+			memory_values.push(val);
+			memory_tile_ids.push(tile.id);
+		} else if(memory_values.length == 1){
+			memory_values.push(val);
+			memory_tile_ids.push(tile.id);
+			if(memory_values[0] == memory_values[1]){
+				tiles_flipped += 2;
+				// Clear both arrays
+				memory_values = [];
+            	memory_tile_ids = [];
+				// Check to see if the whole board is cleared
+				if(tiles_flipped == memory_array.length){
+					alert("You Cleared The Board!!!");
+					document.getElementById('memory_board').innerHTML = "";
+					newBoard();
+				}
+			} else {
+				function flip2Back(){
+				    // Flip the 2 tiles back over
+				    var tile_1 = document.getElementById(memory_tile_ids[0]);
+				    var tile_2 = document.getElementById(memory_tile_ids[1]);
+				    tile_1.style.background = 'url(../images/shells.jpg)';
+            	    tile_1.innerHTML = "";
+				    tile_2.style.background = 'url(../images/shells.jpg)';
+            	    tile_2.innerHTML = "";
+				    // Clear both arrays
+				    memory_values = [];
+            	    memory_tile_ids = [];
+				}
+				setTimeout(flip2Back, 700);
+			}
+		}
+	}
+}
+//end of memory game 
---
+++ js/calendar.js
@@ -0,0 +1,11 @@
+// JavaScript File
+
+
+$(document).ready(function() {
+	$('#GoogleIt').click(function() {
+		
+		$('#GoogleIt').effect('slide',
+		{direction:'right'}, 2000);
+	});
+});
+
